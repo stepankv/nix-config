37d2ef7f58d4e01b1ed2ba0429d1ccfebfecd155
Stepan Kruglov <24195348+stepankv@users.noreply.github.com>
2026-06-03 08:02:01 +0300

    chore(hm): Boot from grub

--- hosts/nixos/configuration.nix
+++ hosts/nixos/configuration.nix
@@ -1,7 +1,13 @@
 { pkgs, station, ... }:
 
 {
-  boot.loader.systemd-boot.enable = true;
+  boot.loader.grub = {
+    enable = true;
+    device = "nodev";
+    efiSupport = true;
+    useOSProber = true;
+  };
+
   boot.loader.efi.canTouchEfiVariables = true;
 
   networking = {
