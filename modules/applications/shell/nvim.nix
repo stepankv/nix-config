76cc1ac62bd541af84b6e86bf7e36e8605911d3f
Stepan Kruglov <24195348+stepankv@users.noreply.github.com>
2026-07-06 20:00:02 +0300

    chore(hm): Install LS servers

--- modules/applications/shell/nvim.nix
+++ modules/applications/shell/nvim.nix
@@ -3,6 +3,7 @@
 {
   home.packages = with pkgs.unstable; [
     neovim
+
     xclip # used by Nvim to share OS and Nvim buffers (run `:h clipboard` for details)
     libxml2 # XML formatter
     shellcheck # Shell linter. Used by bash-language-server.
@@ -12,6 +13,19 @@
     jq # JSON file formatter
     tree-sitter # Syntax highlighting
     nixfmt # nix language formatter
+    taplo # Toml language formatter
+
+    # Language Servers
+    lua-language-server
+    bash-language-server
+    buf
+    yaml-language-server
+    vscode-json-languageserver
+    just-lsp
+    typescript-language-server
+    basedpyright
+    ruff
+    nil # nix language server
   ];
 
   xdg.configFile."nvim".source = "${dotfiles-nvim}";
